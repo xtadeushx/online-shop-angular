f238e05af23ed284e42fa9429fa2228479980975
xtadeushx <alekscherkasov13@gmail.com>
2023-07-14 15:44:21 +0300

    feat: implement base template

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BaseComponent } from './components/base/base.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { BasketComponent } from './components/basket/basket.component';
+
+const routes: Routes = [
+  { path: '', component: BaseComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'product/:id', component: ProductDetailsComponent },
+  { path: 'basket', component: BasketComponent },
+  { path: '**', redirectTo: '', component: BasketComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/UI/footer/footer.component';
+import { HeaderComponent } from './components/UI/header/header.component';
+import { ProductsComponent } from './components/products/products.component';
+import { BasketComponent } from './components/basket/basket.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { DialogBoxComponent } from './components/dialog-box/dialog-box.component';
+import { BaseComponent } from './components/base/base.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    ProductsComponent,
+    BasketComponent,
+    ProductDetailsComponent,
+    DialogBoxComponent,
+    BaseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
