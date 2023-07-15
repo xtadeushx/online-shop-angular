b06c7536e684d6e345a501912e862f0f491f0548
xtadeushx <alekscherkasov13@gmail.com>
2023-07-15 15:25:58 +0300

    feat: implement mapping data to products component

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { IProduct } from 'src/app/models/models.interface';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -7,7 +10,18 @@ import { Component } from '@angular/core';
 })
 
 
-export class ProductsComponent {
-  Image: string = '../../../assets/images/macbook.jpeg'
+export class ProductsComponent implements OnInit {
+  Image: string = '../../../assets/images/macbook.jpeg';
+  products: IProduct[] = [];
+  productSubscription: Subscription
 
+  constructor(private productService: ProductsService) { }
+
+  ngOnInit(): void {
+    this.productSubscription = this.productService.getProducts().subscribe(data => this.products = data);
+  };
+
+  ngOnDestroy(): void {
+    if (this.productSubscription) this.productSubscription.unsubscribe();
+  };
 }
