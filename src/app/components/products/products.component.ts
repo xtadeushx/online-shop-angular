63f7210ce7a894b6dea4581d0a8633fcb0953423
xtadeushx <alekscherkasov13@gmail.com>
2023-07-17 15:57:57 +0300

    fix: add edit button to product component

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -14,10 +14,11 @@ export class ProductsComponent implements OnInit {
   Image: string = '../../../assets/images/macbook.jpeg';
   products: IProduct[] = [];
   productSubscription: Subscription
-
+  canEdit: boolean = false;
   constructor(private productService: ProductsService) { }
 
   ngOnInit(): void {
+    this.canEdit = true;
     this.productSubscription = this.productService.getProducts().subscribe(data => this.products = data);
   };
 
