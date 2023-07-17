87debb2f9902277f250707ba00d192bd0a341065
xtadeushx <alekscherkasov13@gmail.com>
2023-07-17 15:41:44 +0300

    feat: implement product-details component

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/models/models.interface';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: IProduct
+  productSubscription: Subscription
+  constructor(private route: ActivatedRoute) {
+  }
 
+  ngOnInit(): void {
+    this.productSubscription = this.route.data.subscribe(
+      data => this.product = data['data']
+    )
+  }
 }
